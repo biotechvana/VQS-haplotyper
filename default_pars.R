b0ec5c750f7720b339e86c83464baa5fd4e657c3
Biotechvana <biotechvana@biotechvana.com>
2022-10-21 09:56:39 +0200

    Add files via upload

---
+++ default_pars.R
@@ -0,0 +1,36 @@
+
+################################################
+###   PARÁMETROS DEL PIPELINE VQS-HAPLOTYPER   #
+################################################
+
+stopifnot(suppressMessages(require(Biostrings)))
+stopifnot(suppressMessages(require(stringr)))
+stopifnot(suppressMessages(require(RColorBrewer)))
+
+dataDir <- "./data"
+codeDir <- "./R"
+runDir <- "./run"
+flashDir <- "./flash"
+flashFiltDir <- "./flashFilt"
+splitDir <- "./splits"
+trimDir <- "./trim"
+filtDir <- "./filt"
+joinDir <- "./join"
+repDir <- "./reports"
+resultsDir <- resDir <- "./results"
+exportDir <- expDir <- "./export"
+tempDir <- "./tmp"
+ntDir <- "./nt"
+nt01Dir <- "./nt.01"
+aaDir <- "./aa"
+
+#-----------------------------------------------------------------------#
+
+###  Para splitByPrimers  ###
+
+##  TRIM PRIMERS
+###  Depende de si hay adaptadores 454, MIDs y/o M13
+target.io <- 1   # 10   25   50
+target.in <- 30  # 55   55   80
+
+#-----------------------------------------------------------------------#
