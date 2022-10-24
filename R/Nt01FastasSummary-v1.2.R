11bf7631606ae6882cb345bdc0e58c3100fd6ffe
Biotechvana <biotechvana@biotechvana.com>
2022-10-24 16:26:19 +0200

    Update Nt01FastasSummary-v1.2.R

--- R/Nt01FastasSummary-v1.2.R
+++ R/Nt01FastasSummary-v1.2.R
@@ -80,7 +80,7 @@ fl.patt <- "\\.fna$"
 
 flnms <- list.files(path=nt01Dir,patt=fl.patt)
 parts <- t(sapply(flnms,function(str) strsplit(str,split="\\.")[[1]]))
-anms <- parts[,4]
+anms <- parts[,2]
 snms <- parts[,1]
 
 n <- length(flnms)
