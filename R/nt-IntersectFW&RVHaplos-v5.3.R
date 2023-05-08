b94c7b3f8439052dc1e098f09b1e58c72e09d159
Biotechvana <biotechvana@biotechvana.com>
2023-05-08 13:15:51 +0200

    Update nt-IntersectFW&RVHaplos-v5.3.R

--- R/nt-IntersectFW&RVHaplos-v5.3.R
+++ R/nt-IntersectFW&RVHaplos-v5.3.R
@@ -326,7 +326,7 @@ for(i in 1:n)
 
    ###  Report point mutations
    if(length(lst$bseqs)>1)
-   { muts <- SummaryMuts.w(lst$bseqs,lst$nr,off,RefSeqs[samples$Primer.idx][i][[1]])
+   { muts <- SummaryMuts.w(lst$bseqs,lst$nr,off,RefSeqs[pr.idx][[1]])
      cat("\nObserved point mutations:\n")
      print(muts)
      cat("\n")
