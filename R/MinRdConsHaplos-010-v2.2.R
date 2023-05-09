4654e068560fab416b44a7a81dff9b720f67e40a
Biotechvana <biotechvana@biotechvana.com>
2023-05-09 19:20:07 +0200

    Update MinRdConsHaplos-010-v2.2.R

--- R/MinRdConsHaplos-010-v2.2.R
+++ R/MinRdConsHaplos-010-v2.2.R
@@ -104,7 +104,8 @@ for(i in 1:length(in.files))
   if(length(lr$seqs)>1)
   { pr.idx <- FlTbl$Pr.ID[i]
     off <- primers$FW.tpos[pr.idx]-1
-    muts <- SummaryMuts.w(lr$seqs,lr$df$reads,off,RefSeqs[pr.idx][[1]])
+    refseq.idx <- which(grepl(FlTbl$Ampl.Nm[i], names(RefSeqs)))
+    muts <- SummaryMuts.w(lr$seqs,lr$df$reads,off,RefSeqs[refseq.idx][[1]])
     cat("\nObserved point mutations:\n")
     print(muts)
   } else if(length(lr$seqs) == 1)
